577fdd21e1f30f9d5817bfc5f958c562d9a7e2e2
kirillbarkhatov <k.s.barkhatov@gmail.com>
2024-12-04 01:01:31 +0300

    приложения

--- config/settings.py
+++ config/settings.py
@@ -14,6 +14,7 @@ import os
 import dotenv
 from pathlib import Path
 
+from django.conf.global_settings import AUTH_USER_MODEL
 from dotenv import load_dotenv
 
 
@@ -46,7 +47,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
-    "rest_framework"
+    "rest_framework",
+    "lms",
+    "users"
 ]
 
 MIDDLEWARE = [
@@ -135,3 +138,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = "users.User"
