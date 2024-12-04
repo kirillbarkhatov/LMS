91805e87d658123bcdbeb300ee23c273fca5d698
kirillbarkhatov <k.s.barkhatov@gmail.com>
2024-12-04 23:34:47 +0300

    контроллеры lms

--- lms/views.py
+++ lms/views.py
@@ -1,3 +1,44 @@
-from django.shortcuts import render
+from rest_framework import viewsets, generics
+from .models import Course, Lesson
+from .serializers import CourseSerializer, LessonSerializer
 
-# Create your views here.
+
+class CourseViewSet(viewsets.ModelViewSet):
+    """Вьюсет для курсов"""
+
+    model = Course
+    serializer_class = CourseSerializer
+
+
+class LessonCreateApiView(generics.CreateAPIView):
+    """Создание урока"""
+
+    serializer_class = LessonSerializer
+
+
+class LessonListApiView(generics.ListAPIView):
+    """Список уроков"""
+
+    serializer_class = LessonSerializer
+    queryset = Lesson.objects.all()
+
+
+class LessonRetrieveAPIView(generics.RetrieveAPIView):
+    """Получить один урок"""
+
+    serializer_class = LessonSerializer
+    queryset = Lesson.objects.all()
+
+
+class LessonRetrieveAPIView(generics.UpdateAPIView):
+    """Обновить урок"""
+
+    serializer_class = LessonSerializer
+    queryset = Lesson.objects.all()
+
+
+class LessonDestroyAPIView(generics.DestroyAPIView):
+    """Удалить урок"""
+
+    serializer_class = LessonSerializer
+    queryset = Lesson.objects.all()
