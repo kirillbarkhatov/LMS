4c54cbaf57d4a969830922bea69125f7bb471377
kirillbarkhatov <k.s.barkhatov@gmail.com>
2024-12-04 23:11:22 +0300

    модели

--- lms/models.py
+++ lms/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
-# Create your models here.
+
+class Course(models.Model):
+    """Модель для курса"""
+
+    name = models.CharField(max_length=50, verbose_name="Название курса")
+    preview = models.ImageField(upload_to="lms/course", blank=True, null=True, verbose_name="Превью курса")
+    description = models.TextField(blank=True, null=True, verbose_name="Описание курса")
+
+    class Meta:
+        verbose_name = "Курс"
+        verbose_name_plural = "Курсы"
+
+
+class Lesson(models.Model):
+    """Модель для уроков"""
+
+    name = models.CharField(max_length=50, verbose_name="Название урока")
+    preview = models.ImageField(upload_to="lms/course", blank=True, null=True, verbose_name="Превью урока")
+    description = models.TextField(blank=True, null=True, verbose_name="Описание урока")
+    url = models.CharField(max_length=300, verbose_name="Ссылка на видео")
+
+    class Meta:
+        verbose_name = "Урок"
+        verbose_name_plural = "Уроки"
