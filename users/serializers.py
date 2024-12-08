29470730c89dfa577bff9e7cbc3daa136397ea8b
kirill_barkhatov <k.s.barkhatov@gmail.com>
2024-12-08 21:38:12 +0300

    вывод платежей для пользователя

--- users/serializers.py
+++ users/serializers.py
@@ -3,17 +3,19 @@ from rest_framework.serializers import ModelSerializer
 from .models import User, Payment
 
 
-class UserSerializer(ModelSerializer):
-    """Сериализатор для пользователя"""
-
-    class Meta:
-        model = User
-        fields = "__all__"
-
-
 class PaymentSerializer(ModelSerializer):
     """Сериализатор для платежей"""
 
     class Meta:
         model = Payment
+        fields = "__all__"
+
+
+class UserSerializer(ModelSerializer):
+    """Сериализатор для пользователя"""
+
+    payments = PaymentSerializer(many=True)
+
+    class Meta:
+        model = User
         fields = "__all__"
