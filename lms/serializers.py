3513ae7d98f779a8f9b362aab4e84ab7295a15b9
kirill_barkhatov <k.s.barkhatov@gmail.com>
2024-12-08 20:59:06 +0300

    вывод полной информации об уроках внутри курса

--- lms/serializers.py
+++ lms/serializers.py
@@ -3,22 +3,23 @@ from rest_framework.serializers import ModelSerializer, SerializerMethodField
 from .models import Course, Lesson
 
 
+class LessonSerializer(ModelSerializer):
+    """Сериализатор для уроков"""
+
+    class Meta:
+        model = Lesson
+        fields = "__all__"
+
+
 class CourseSerializer(ModelSerializer):
     """Сериализатор для курсов"""
 
     lessons_count = SerializerMethodField()
+    lessons = LessonSerializer(many=True)
 
     def get_lessons_count(self, course):
         return course.lessons.count()
 
     class Meta:
         model = Course
-        fields = "__all__"
-
-
-class LessonSerializer(ModelSerializer):
-    """Сериализатор для уроков"""
-
-    class Meta:
-        model = Lesson
         fields = "__all__"
