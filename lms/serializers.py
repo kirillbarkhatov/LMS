a296655e7aea56b26cc148937fb2e1572fbaf709
kirillbarkhatov <k.s.barkhatov@gmail.com>
2024-12-16 23:13:26 +0300

    валидатор в сериализатор

--- lms/serializers.py
+++ lms/serializers.py
@@ -1,20 +1,23 @@
-from rest_framework.serializers import ModelSerializer, SerializerMethodField
+from rest_framework import serializers
 
 from .models import Course, Lesson
+from .validators import validate_lesson_url
 
 
-class LessonSerializer(ModelSerializer):
+class LessonSerializer(serializers.ModelSerializer):
     """Сериализатор для уроков"""
+
+    url = serializers.URLField(validators=[validate_lesson_url])
 
     class Meta:
         model = Lesson
         fields = "__all__"
 
 
-class CourseSerializer(ModelSerializer):
+class CourseSerializer(serializers.ModelSerializer):
     """Сериализатор для курсов"""
 
-    lessons_count = SerializerMethodField()
+    lessons_count = serializers.SerializerMethodField()
     lessons = LessonSerializer(many=True, read_only=True)
 
     def get_lessons_count(self, course):
