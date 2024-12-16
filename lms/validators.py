27b042a2689041447dcf03aa8f39a7642bd5f030
kirillbarkhatov <k.s.barkhatov@gmail.com>
2024-12-16 22:17:20 +0300

    валидатор уроков

---
+++ lms/validators.py
@@ -0,0 +1,7 @@
+from rest_framework import serializers
+
+def validate_lesson_url(value):
+    """Валидатор ссылок на уроки"""
+
+    if "youtube.com" not in value:
+        raise serializers.ValidationError("Допускаются только ссылки на youtube.com")с
